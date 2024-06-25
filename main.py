e9df07669c64673a02ff5910d780b15a5d85cd4a
itsmeayushipanday <ayushipanday22@gmail.com>
2024-06-26 00:03:35 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class hello:
+    def __init__(self, fname, lname, age):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+obj = hello("ayushi", "panday", 22)
+print(obj.fname)
+print(obj.lname)
+print(obj.age)
